68c01b6d9214b23432a26137d611bcd5f6f04a18
MBurakT <mbtbrk11@outlook.com>
2024-02-09 18:41:15 +0300

    Update WebApi/Entities/Movie.cs

--- WebApi/DBOperations/Migrations/MovieSoftDelete/20240209153824_MovieSoftDelete.Designer.cs
+++ WebApi/DBOperations/Migrations/MovieSoftDelete/20240209153824_MovieSoftDelete.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApi.DBOperations;
 
 #nullable disable
 
-namespace WebApi.DBOperations.Migrations.InitialMigration
+namespace WebApi.DBOperations.Migrations.MovieSoftDelete
 {
     [DbContext(typeof(MovieStoreDbContext))]
-    partial class MovieStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240209153824_MovieSoftDelete")]
+    partial class MovieSoftDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
